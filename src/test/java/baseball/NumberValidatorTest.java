6649ef450780f26eca83238705551f65bc38a1fa
K-KY <kyuyoungk@naver.com>
2023-11-29 17:21:08 +0900

    Test(NumberValidatorTest) : 중복, 숫자 갯수 테스트

--- src/test/java/baseball/NumberValidatorTest.java
+++ src/test/java/baseball/NumberValidatorTest.java
@@ -3,31 +3,51 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class NumberValidatorTest {
 
     @Test
     void isNotNumber() {
-        NumberValidator numberValidator = new NumberValidator();
-        assertThatThrownBy(() -> numberValidator.isNumber("a")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> NumberValidator.isNumber("a")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void isNumber() {
-        NumberValidator numberValidator = new NumberValidator();
-        assertThatCode(() -> numberValidator.isNumber("1")).doesNotThrowAnyException();
+        assertThatCode(() -> NumberValidator.isNumber("1")).doesNotThrowAnyException();
     }
 
     @Test
     void isNotInRange() {
-        NumberValidator numberValidator = new NumberValidator();
-        assertThatThrownBy(() -> numberValidator.isInRange(10)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> NumberValidator.isInRange(List.of(10, 11, 12))).isInstanceOf(
+                IllegalArgumentException.class);
     }
 
     @Test
     void isInRange() {
-        NumberValidator numberValidator = new NumberValidator();
-        assertThatCode(() -> numberValidator.isInRange(9)).doesNotThrowAnyException();
+        assertThatCode(() -> NumberValidator.isInRange(List.of(1, 2, 3))).doesNotThrowAnyException();
+    }
+
+    @Test
+    void isDuplicate() {
+        assertThatThrownBy(() -> NumberValidator.isDuplicate(List.of(1, 1, 2))).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    void isNotDuplicate() {
+        assertThatCode(() -> NumberValidator.isDuplicate(List.of(1, 3, 2))).doesNotThrowAnyException();
+    }
+
+    @Test
+    void isInAmount() {
+        assertThatCode(() -> NumberValidator.isInAmount(List.of(1, 3, 2))).doesNotThrowAnyException();
+    }
+
+    @Test
+    void isNotInAmount() {
+        assertThatThrownBy(() -> NumberValidator.isInAmount(List.of(1, 3, 2, 5))).isInstanceOf(
+                IllegalArgumentException.class);
     }
 }
