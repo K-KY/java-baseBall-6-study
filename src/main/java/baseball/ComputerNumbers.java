0e819ab8b3770e37c5c0bb47c85253c374211d4a
K-KY <kyuyoungk@naver.com>
2023-11-29 17:34:53 +0900

    Refactor(ComputerNumbers) : 상수화

--- src/main/java/baseball/ComputerNumbers.java
+++ src/main/java/baseball/ComputerNumbers.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class ComputerNumbers implements BallSeparator {
+    private static final String NOTHING = "낫싱";
+    private static final int START = 0;
+    private static final int END = 2;
     private final List<Ball> computerBalls;
 
     public ComputerNumbers(List<Integer> generated) {
@@ -14,12 +17,15 @@ public class ComputerNumbers implements BallSeparator {
     @Override
     public List<Ball> separator(List<Integer> generated) {
         List<Ball> balls = new ArrayList<>();
-        IntStream.rangeClosed(0, 2).forEach(num -> balls.add(new Ball(num, generated.get(num))));
+        IntStream.rangeClosed(START, END).forEach(num -> balls.add(new Ball(num, generated.get(num))));
         return balls;
     }
 
     public String score(Ball userBall) {
-        return computerBalls.stream().map(ball -> ball.score(userBall)).filter(score -> !score.equals("낫싱")).findFirst()
-                .orElse("낫싱");
+        return computerBalls.stream()
+                .map(ball -> ball.score(userBall))
+                .filter(score -> !score.equals(NOTHING))
+                .findFirst()
+                .orElse(NOTHING);
     }
 }
