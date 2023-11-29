635a33711bd1874a530e68e077f364cd4c94d418
K-KY <kyuyoungk@naver.com>
2023-11-29 17:35:44 +0900

    Refactor(UserInput) :  메세지 상수화

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -7,10 +7,15 @@ import java.util.stream.Collectors;
 
 public class UserInput {
 
+    private static final String INPUT_BALL_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String END_OR_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String REGEX = "";
+
     public List<Integer> ballNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_BALL_NUMBER);
         String input = Console.readLine();
-        List<String> userInput = new ArrayList<>(List.of(input.split("")));
+        List<String> userInput = new ArrayList<>(List.of(input.split(REGEX)));
         userInput.forEach(NumberValidator::isNumber);
         return userInput.stream()
                 .mapToInt(Integer::parseInt)
@@ -19,8 +24,8 @@ public class UserInput {
     }
 
     public int endOrRestart() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
-                + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_END
+                + END_OR_RESTART);
         String input = Console.readLine();
         int number = NumberValidator.isNumber(input);
         NumberValidator.isInRange(number);
