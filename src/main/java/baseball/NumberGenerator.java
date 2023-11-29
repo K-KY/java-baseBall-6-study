8ef8006a9ec8ec5aa09bcd2c3e3509b8e6e96f5f
K-KY <kyuyoungk@naver.com>
2023-11-29 17:35:08 +0900

    Refactor(NumberGenerator) : 매직넘버 상수화

--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -6,16 +6,20 @@ import java.util.List;
 
 public class NumberGenerator {
     private static final List<Integer> generatedNumber = new ArrayList<>();
+    private static final int LIMIT = 3;
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+
     public static List<Integer> generateNumber() {
         generatedNumber.clear();
-        while (generatedNumber.size() < 3) {
+        while (generatedNumber.size() < LIMIT) {
             generate();
         }
         return generatedNumber;
     }
 
     private static void generate() {
-        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        int randomNumber = Randoms.pickNumberInRange(START_RANGE, END_RANGE);
         if (!generatedNumber.contains(randomNumber)) {
             generatedNumber.add(randomNumber);
         }
