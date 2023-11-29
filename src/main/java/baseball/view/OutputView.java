64c263687d0c709870433b81258dbff9d297024c
K-KY <kyuyoungk@naver.com>
2023-11-29 17:35:34 +0900

    Refactor(OutputView) :  메세지 상수화

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,26 +5,32 @@ import java.util.List;
 
 public class OutputView {
 
+    private static final String NOTHING = "낫싱";
+    private static final String NULL = "";
+    private static final String BALL = "볼 ";
+    private static final int ZERO = 0;
+    private static final String STRIKE = "스트라이크";
+
     public static void printResult(List<String> scores) {
         int ball = (int) scores.stream().filter(BaseBallScore::isBall).count();
         int strike = (int) scores.stream().filter(BaseBallScore::isStrike).count();
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
+        if (ball == ZERO && strike == ZERO) {
+            System.out.println(NOTHING);
         }
         System.out.println(ballScore(ball) + strikeScore(strike));
     }
 
     private static String ballScore(int ball) {
-        if (ball == 0) {
-            return "";
+        if (ball == ZERO) {
+            return NULL;
         }
-        return ball + "볼 ";
+        return ball + BALL;
     }
 
     private static String strikeScore(int strike) {
-        if (strike == 0) {
-            return "";
+        if (strike == ZERO) {
+            return NULL;
         }
-        return strike + "스트라이크";
+        return strike + STRIKE;
     }
 }
