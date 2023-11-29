6a35f67caec6f3cdd6f5d3d2a96477c3d06e1646
K-KY <kyuyoungk@naver.com>
2023-11-29 17:20:12 +0900

    Feat(UserInput) : 사용자로부터 재시작 여부를 입력받는 메서드

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class UserInput {
 
     public List<Integer> ballNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         List<String> userInput = new ArrayList<>(List.of(input.split("")));
         userInput.forEach(NumberValidator::isNumber);
@@ -15,5 +16,14 @@ public class UserInput {
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
+    }
+
+    public int endOrRestart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        int number = NumberValidator.isNumber(input);
+        NumberValidator.isInRange(number);
+        return number;
     }
 }
