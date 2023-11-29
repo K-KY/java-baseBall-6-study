e0e25acab9bcf0107bb03a22c28661d99f79d723
K-KY <kyuyoungk@naver.com>
2023-11-29 16:22:24 +0900

    Feat(NumberValidator) : 중복과 숫자 갯수가 3개인지 검증

--- src/main/java/baseball/NumberValidator.java
+++ src/main/java/baseball/NumberValidator.java
@@ -1,18 +1,33 @@
 package baseball;
 
+import java.util.List;
+
 public class NumberValidator {
     private static final String DIGIT = "[0-9]+";
 
-    public int isNumber(String input) {
+    public static int isNumber(String input) {
         if (!input.matches(DIGIT)) {
             throw new IllegalArgumentException("[ERROR] 올바른 숫자를 입력 해주세요.");
         }
         return Integer.parseInt(input);
     }
 
-    public void isInRange(int number) {
+    public static void isInRange(int number) {
         if (number > 9 || number < 0) {
             throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1부터 9까지 입니다.");
         }
+    }
+
+    public static void isDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복되지 않는 숫자를 입력 해주세요");
+        }
+    }
+
+    public static void isInAmount(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("[ERROR] 3개의 숫자를 입력 해주세요");
+        }
+
     }
 }
